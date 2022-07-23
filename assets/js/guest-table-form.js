3f4c823011aee11c858f35b6ad503d10567cb0c8
Ryan J <ryan_c_jonah@yahoo.ca>
2022-07-23 15:03:37 -0300

    Added generic table functionality (dynamic naming not yet supported)

--- assets/js/guest-table-form.js
+++ assets/js/guest-table-form.js
@@ -5,6 +5,20 @@ function addBridalRow(){
     formSection.appendChild(clon);
 }
 
+function addTable(){
+    var table = document.getElementsByClassName("tableTemplate")[0];
+    var newTable = table.content.cloneNode(true);
+    var tableContainer = document.getElementById("genericTableContainer");
+    tableContainer.appendChild(newTable);
+}
+
+function addRow(tableForm){
+    var row = tableForm.getElementsByClassName("tableRowTemplate")[0];
+    var newRow = row.content.cloneNode(true);
+    var rowContainer = tableForm.getElementsByClassName("genericTableRowContainer")[0];
+    rowContainer.appendChild(newRow);
+}
+
 function removeRow(element){
     element.parentElement.parentElement.remove();
 }
