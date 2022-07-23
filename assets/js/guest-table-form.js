c4a49eddce73e61027f10ca0fe522cb759aa04b8
Ryan J <ryan_c_jonah@yahoo.ca>
2022-07-23 16:20:55 -0300

    Implemented dynamic table creation + table deletion

--- assets/js/guest-table-form.js
+++ assets/js/guest-table-form.js
@@ -1,3 +1,8 @@
+var tableIteration = 1;
+
+/**
+ * Creates a new table within #bridalForm
+ */
 function addBridalRow(){
     var temp = document.getElementsByClassName("bridalTemplate")[0];
     var clon = temp.content.cloneNode(true);
@@ -5,13 +10,28 @@ function addBridalRow(){
     formSection.appendChild(clon);
 }
 
+/**
+ * Creates a new table within #genericTableContainer
+ */
 function addTable(){
+    //Create new table from HTML template
     var table = document.getElementsByClassName("tableTemplate")[0];
     var newTable = table.content.cloneNode(true);
+
+    //Set table title dynamically 
+    var newTableTitle = newTable.querySelector(".genericTableTitle");
+    newTableTitle.innerText = `Table ${tableIteration}`;
+    tableIteration++;
+
+    //Add new table to table container
     var tableContainer = document.getElementById("genericTableContainer");
     tableContainer.appendChild(newTable);
 }
 
+/**
+ * Creates new input field within a table
+ * @param {Container of the current table} tableForm 
+ */
 function addRow(tableForm){
     var row = tableForm.getElementsByClassName("tableRowTemplate")[0];
     var newRow = row.content.cloneNode(true);
@@ -21,4 +41,9 @@ function addRow(tableForm){
 
 function removeRow(element){
     element.parentElement.parentElement.remove();
+}
+
+function removeTable(element){
+    element.parentElement.remove();
+    tableIteration--;
 }
